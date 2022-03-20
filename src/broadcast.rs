62bed49a91f6f2d3d320d669ed484b69b660b5e6
Florian Milcendeau <fmilcendeau85@gmail.com>
2022-03-20 21:08:37 +0100

    refacto

---
+++ src/broadcast.rs
@@ -0,0 +1,36 @@
+use anyhow::Result;
+use std::sync::Arc;
+use webrtc::api::interceptor_registry::register_default_interceptors;
+use webrtc::api::media_engine::MediaEngine;
+use webrtc::api::APIBuilder;
+use webrtc::ice_transport::ice_server::RTCIceServer;
+use webrtc::interceptor::registry::Registry;
+use webrtc::peer_connection::configuration::RTCConfiguration;
+use webrtc::peer_connection::RTCPeerConnection;
+
+pub async fn peer_connection() -> Result<Arc<RTCPeerConnection>, anyhow::Error> {
+    let mut media_engine = MediaEngine::default();
+
+    media_engine.register_default_codecs()?;
+
+    let mut registry = Registry::new();
+
+    registry = register_default_interceptors(registry, &mut media_engine)?;
+
+    let api = APIBuilder::new()
+        .with_media_engine(media_engine)
+        .with_interceptor_registry(registry)
+        .build();
+
+    let config = RTCConfiguration {
+        ice_servers: vec![RTCIceServer {
+            urls: vec!["stun:stun.l.google.com:19302".to_owned()],
+            ..Default::default()
+        }],
+        ..Default::default()
+    };
+
+    let peer_connection = Arc::new(api.new_peer_connection(config).await?);
+
+    return Ok(peer_connection);
+}
