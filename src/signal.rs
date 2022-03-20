4ee5d1175e42a394e54ee1bf4bd05b63c009c5f0
FlorianMilcendeau <fmilcendeau85@gmail.com>
2022-03-20 11:16:45 +0100

    feature/signal (#1)
    * feature/signal/encode-decode

---
+++ src/signal.rs
@@ -0,0 +1,30 @@
+use anyhow::Result;
+
+/// TODO - compress
+pub fn encode(s: &str) -> String {
+    base64::encode(s)
+}
+
+/// TODO - decompress
+pub fn decode(s: &str) -> Result<String> {
+    let b = base64::decode(s)?;
+
+    Ok(String::from_utf8(b)?)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn encode_test() {
+        let str_encoded = encode("Hello world");
+        assert_eq!(str_encoded, "SGVsbG8gd29ybGQ=");
+    }
+
+    #[test]
+    fn decode_test() {
+        let str_decoded = decode("SGVsbG8gd29ybGQ=").unwrap();
+        assert_eq!(str_decoded, "Hello world");
+    }
+}
